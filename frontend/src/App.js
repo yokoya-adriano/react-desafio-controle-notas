fd5cd6dea74241a1962e3900feb37e683c2f4bbb
Adriano Yokoya <adriano_yokoya@hotmail.com>
2020-10-29 10:10:51 -0300

    Add grades
    was add the grades on a table on frontend for further  modification to user.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import * as api from './api/apiService';
+import GradesControl from './components/GradesControl';
+import Spinner from './components/Spinner';
 
 export default function App() {
-  const testApi = async () => {
-    const result = await api.getAllGrades();
-    console.log(result);
+  //todas as notas do back
+  const [allGrades, setAllGrades] = useState([]);
+  //nota que o usuário escolheu para editar
+  const [selectedGrade, setSelectedGrade] = useState({});
+  //começando com false para optar em abrir ou não o modal
+  const [isModal, setIsModal] = useState(false);
+
+  //Effect para trazer os dados da api
+  useEffect(() => {
+    const getGrades = async () => {
+      const grades = await api.getAllGrades();
+      setTimeout(() => {
+        setAllGrades(grades);
+      }, 2000);
+    };
+    // api.getAllGrades().then((grades) => {
+    //   setTimeout(() => {
+    //     setAllGrades(grades);
+    //   }, 2000);
+    // });
+    getGrades();
+  }, []);
+
+  const handleDelete = () => {
+    console.log('handleDelete');
   };
 
-  testApi();
+  const handlePersist = () => {
+    console.log('handleDelete');
+  };
 
-  return <h1>Olá</h1>;
+  return (
+    <div>
+      <h1 className="center">Controle de notas</h1>
+
+      {allGrades.length == 0 && <Spinner />}
+      {allGrades.length > 0 && (
+        <GradesControl
+          grades={allGrades}
+          onDelete={handleDelete}
+          onPersist={handlePersist}
+        />
+      )}
+    </div>
+  );
 }
---
+++ frontend/src/components/GradesControl.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function GradesControl({ grades, onDelete, onPersist }) {
+  return (
+    <div className="container">
+      <table className="striped center">
+        <thead>
+          <tr>
+            <th>Aluno</th>
+            <th>Disciplina</th>
+            <th>Avaliação</th>
+            <th>Nota</th>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {grades.map(({ id, subject, student, type, value, isDeleted }) => {
+            return (
+              <tr key={id}>
+                <td>{student}</td>
+                <td>{subject}</td>
+                <td>{type}</td>
+                <td>{value}</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+            );
+          })}
+          )
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
+}
